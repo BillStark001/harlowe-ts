44c37b2fe57904abc9f93fd690cd12b8e2c4534b
Bill Stark <31788509+BillStark001@users.noreply.github.com>
2023-04-23 18:30:27 +0900

    adding executable files & test files

---
+++ exec/main.ts
@@ -0,0 +1,68 @@
+import CodeSlicer from '../src/toolbox/slicer';
+import Markup from '../src/markup';
+import { parse } from 'ts-command-line-args';
+
+interface HarloweToolArgs {
+  src: string;
+  rec?: string;
+  dst?: string;
+  slice: boolean;
+  recv: boolean;
+  help: boolean;
+  include?: string[];
+  exclude?: string[];
+}
+
+console.log(process.argv);
+console.log(parse<HarloweToolArgs>({
+  src: { type: String, alias: 's' },
+  dst: { type: String, optional: true, alias: 'd' },
+  rec: { type: String, optional: true, alias: 'r', },
+  slice: { type: Boolean },
+  recv: { type: Boolean },
+  help: { type: Boolean, alias: 'h', description: 'Prints this usage guide' },
+  include: { type: String, multiple: true, optional: true, alias: 'n' },
+  exclude: { type: String, multiple: true, optional: true, alias: 'x' },
+},{
+  helpArg: 'help',
+  headerContentSections: [{ header: 'Harlowe Toolbox', content: 'Thanks for using Our Library' }],
+},));
+
+
+const testPassage = `
+Or, type some Harlowe code in this text area, and click ▶ to see it render.
+
+\\(set: _style to (text-style:"buoy"), $word to "this")(enchant:$word, _style)\\
+
+<img src="test src" alt="test alt">
+
+
+
+
+<!--test1-->
+
+
+
+
+
+This panel ↓ will show any variables that are set by the code, as well as any enchantments applied to the prose using enchantment macros like \`(enchant:)\`.
+
+Press "Debug View" and then click the 🔍 icons to see a step-by-step guide of how Harlowe processed a macro.
+
+[[slicer test -> slicer target]]
+
+
+<!--test2-->
+`;
+
+const ast = Markup.lex(testPassage);
+
+const pieces = CodeSlicer.slice(testPassage, {
+  included: ['text', 'tag'],
+  skipped: [],
+  withTypeRecord: true,
+});
+
+for (const piece of pieces) {
+  console.log(piece);
+}
